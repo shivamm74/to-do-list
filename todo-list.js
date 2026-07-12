bb8e1c6713248b32afb204c2336760ed0c1a1edd
shivamm74 <shivamsharmaa9594@gmail.com>
2026-07-12 22:09:42 +0530

    improved the todo list

--- todo-list.js
+++ todo-list.js
@@ -5,7 +5,17 @@ function addItem() {
 
    const buttonElement =  document.querySelector('.input');
    const buttonValue = buttonElement.value;
+   let todolisthtml = '';
+   
    ToDoList.push(buttonValue);
    console.log(ToDoList)
+   for (let i = 0 ; i < ToDoList.length ; i++){
+      const todo = ToDoList[i];
+      const phra2 = `<p>${todo}<p>`
+      todolisthtml += phra2
+
+   }
+   console.log(todolisthtml)
+
    buttonElement.value = '';
 }
