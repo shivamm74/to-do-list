412e590f7c67fe204070f9d8a30621fa81be27e7
shivamm74 <shivamsharmaa9594@gmail.com>
2026-08-03 23:39:50 +0530

    used eventlisteners for delete  button

--- todo-list.js
+++ todo-list.js
@@ -44,13 +44,20 @@ function addItem() {
    
       const task = todoobject.task;
       const duedate = todoobject.duedate;
-      const phra2 = `<div class="todo-item"><p class="task-text"><button class = "serial-number">${[i+1]}.</button>${task}</p><p class="date-text">${duedate}</p><button type="button" class="button2" aria-label="Delete task" onclick="ToDoList.splice(${i},1);rendertodolist()"></button></div>`
+      const phra2 = `<div class="todo-item"><p class="task-text"><button class = "serial-number">${[i+1]}.</button>${task}</p><p class="date-text">${duedate}</p><button type="button" class="button2" aria-label="Delete task" ></button></div>`
       todolisthtml += phra2});
 
    
 
    console.log(todolisthtml)
    document.querySelector('.phra1').innerHTML = todolisthtml;
+
+   document.querySelectorAll('.button2').forEach((deletebutton,index) => {
+      deletebutton.addEventListener('click', () => {
+         ToDoList.splice(index,1);
+         rendertodolist();
+      })
+   })
    localStorage.setItem('tasks',JSON.stringify(ToDoList));
    
 }
